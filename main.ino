5a79f07f8a9a2e23b43d92f5c88d584449290522
Pallandoo <pkraak@outlook.com>
2020-06-10 22:19:12 +0200

    Create main.ino
    First template

---
+++ main.ino
@@ -0,0 +1,15 @@
+//All libraries needed to run this code 
+#include <example-lib.h>
+
+// Global variables (can be used in anywhere in the program)
+int sensor = A0;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+}
