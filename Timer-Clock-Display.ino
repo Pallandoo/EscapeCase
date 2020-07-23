a268bf1685527dabc3338b530cbd7ef7b707feb6
Pallandoo <pkraak@outlook.com>
2020-07-23 17:15:57 +0200

    Update Timer-Clock-Display.ino

--- Timer-Clock-Display.ino
+++ Timer-Clock-Display.ino
@@ -1,34 +1,40 @@
+// Display Lib 
 #include <Wire.h> // Display lib
 #include <LiquidCrystal_I2C.h> //Display lib
+LiquidCrystal_I2C lcd(0x20,20,4);  // set the LCD address to 0x20 for a 20 chars and 4 line display
+//LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+// Display Lib END 
 
+// countdown vars
 int S = 59; // count seconds 
 int M = 60; // count minutes
 //int H = 1; // count hours
 bool gamestart = false ;
-
-
-LiquidCrystal_I2C lcd(0x20,20,4);  // set the LCD address to 0x20 for a 20 chars and 4 line display
-//LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+// countdown vars END
 
 
 void setup() {
-  lcd.init();  // initialize the lcd 
-  lcd.begin(); 
-  lcd.backlight();
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Welkom bij de ADR")
-  lcd.setCursor(0,1);
-  lcd.print("Escape Case");
-  lcd.setCursor(0,2);
-  lcd.print("Klik op de knop om")
-  lcd.setCursor(0,3);
-  lcd.print("te beginnen ");
+  initLCD();
 }
 
 void loop() {
   start_countdown_timer();
   update_countdown();
+}
+
+void initLCD() {
+  lcd.init();  // initialize the lcd 
+  //lcd.begin(); 
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Welkom bij de ADR");
+  lcd.setCursor(0,1);
+  lcd.print("Escape Case");
+  lcd.setCursor(0,2);
+  lcd.print("Klik op de knop om");
+  lcd.setCursor(0,3);
+  lcd.print("te beginnen ");
 }
 
 int return_time_left() {
@@ -84,5 +90,6 @@ void update_countdown(){
        lcd.setCursor(12,1);
        lcd.print(" ");
      }
+    }
   }
 }
